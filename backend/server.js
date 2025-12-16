686e4f15782d29bca9fe1464cc0e84b9faf913fe
Harsh Vardhan Jha <harsh.jha@attentive.ai>
2025-12-16 18:31:06 +0530

    origin-error

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,10 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-   origin: [
-      "http://localhost:5173",
-      "https://note-taker-frontend-agyd.onrender.com",
-    ],
+  origin: "https://note-taker-frontend-agyd.onrender.com",
   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 app.use('/user', userRoute);
